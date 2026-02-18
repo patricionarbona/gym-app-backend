3f5be590403752c8b45d2cd95e83f780c2a5048d
patricionarbona <patricioinarbona@gmail.com>
2026-02-18 22:46:13 +0100

    feat add jwt types

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
 import { CreateUserParams } from "../interfaces";
 import { createUserDB } from "./database.controller";
+import jwt from 'jsonwebtoken'
 
+const createToken = () => {
+    jwt.sign
+}
 
 export const signup = async (req: Request, res: Response) => {
     const newUserData:CreateUserParams = req.body
     createUserDB(newUserData)
+
+
     res.json("signup")
 };
 
