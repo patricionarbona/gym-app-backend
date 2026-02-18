d07a6ae66c79fefcbdcee4000553ce703b55aef5
patricionarbona <patricioinarbona@gmail.com>
2026-02-18 19:06:20 +0100

    delete login from routes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,12 @@
 import express from "express";
 import { Request, Response } from "express";
 import morgan from "morgan";
-import loginRoute from "./routes/login";
 import authRoutes from './routes/auth.routes'
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-
-app.use(loginRoute);
 
 app.post("/isAlive", (req: Request, res: Response) => {
   console.log("Server is alive");
