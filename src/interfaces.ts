e06ce7bf2642754284b7701ebaebf8e8540da762
patricionarbona <patricioinarbona@gmail.com>
2026-02-18 19:06:58 +0100

    create userParams & type CreateUserParams

---
+++ src/interfaces.ts
@@ -0,0 +1,16 @@
+export interface UserParams {
+  usuario: string;
+  correo: string;
+  password: string;
+
+  admin?: boolean;
+  created_at?: string;
+  updated_at?: string;
+  last_active_at?: string | null;
+  deleted_at?: string | null;
+}
+
+export type CreateUserParams = Omit<
+  UserParams,
+  "created_at" | "updated_at" | "last_active_at" | "deleted_at"
+>;
