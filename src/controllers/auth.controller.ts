bf676295b4c8f24ad16582f9699f0341a7995fda
patricionarbona <patricioinarbona@gmail.com>
2026-02-20 14:55:47 +0100

    chore: use prettier

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,33 +2,39 @@ import { Request, Response } from "express";
 import { CreateUserParams } from "../interfaces";
 import { createUserDB } from "../services/user.service";
 import jwt from "jsonwebtoken";
-import bcrypt from 'bcryptjs'
-
+import bcrypt from "bcryptjs";
 
 export const encryptPassword = async (password: string) => {
-  const salt = await bcrypt.genSalt(10)
-  return await bcrypt.hash(password, salt)
-}
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
 
-export const comparePassword = async (password: string, receivedPassword: string) => {
-  return await bcrypt.compare(password, receivedPassword)
-}
+export const comparePassword = async (
+  password: string,
+  receivedPassword: string,
+) => {
+  return await bcrypt.compare(password, receivedPassword);
+};
 
 export const signup = async (req: Request, res: Response) => {
   const newUserData: CreateUserParams = req.body;
   const result = await createUserDB(newUserData);
-  if(result.success) {
-    const token = jwt.sign({ id: result.insertId }, process.env.JWT_SECRET as string, {
-      expiresIn: "1h",
-    });
-  
-    return res.json({token: token});
+  if (result.success) {
+    const token = jwt.sign(
+      { id: result.insertId },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: "1h",
+      },
+    );
+
+    return res.json({ token: token });
   }
-  
-  if(result.errorCode === "ER_DUP_ENTRY") {
-    return res.status(409).json({error: "Correo ya registrado"})
+
+  if (result.errorCode === "ER_DUP_ENTRY") {
+    return res.status(409).json({ error: "Correo ya registrado" });
   }
-  return res.status(400).json({error: result.errorCode})
+  return res.status(400).json({ error: result.errorCode });
 };
 
 export const signin = async (req: Request, res: Response) => {
