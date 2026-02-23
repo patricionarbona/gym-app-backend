18b0b15d26e8231e983d59180c0dd5b002608a18
patricionarbona <patricioinarbona@gmail.com>
2026-02-23 14:34:02 +0100

    feat: get user by id

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,4 +1,5 @@
 import { pool } from "../database";
+import { UserDB } from "../interfaces";
 
 
 export const testConnection = async () => {
@@ -10,3 +11,14 @@ export const testConnection = async () => {
   }
 };
 
+export const foundUserById = async (id: number): Promise<UserDB | null> => {
+  try {
+    const query = 'SELECT * FROM usuarios WHERE id = ?'
+    const [rows] = await pool.execute<UserDB[]>(query, [id])
+    console.log(rows)
+    return rows.length ? rows[0] : null
+  } catch( err: any) {
+    console.log("DB Error:", err.message)
+    return null
+  }
+}
