c82959bf2f8f0a046935c4f114a5a4ae15fca40a
patricionarbona <patricioinarbona@gmail.com>
2026-02-23 14:36:36 +0100

    add verifyToken to signin

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as authCtrl from "../controllers/auth.controller";
+import { verifyToken } from "../middlewares/authJwt";
 
 const router = Router();
 
 router.post("/signup", authCtrl.signup);
 
-router.post("/signin", authCtrl.signin);
+router.post("/signin",verifyToken, authCtrl.signin);
 
 export default router;
