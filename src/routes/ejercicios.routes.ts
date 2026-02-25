ae2443211841cb203989bd12785171d2362040ce
patricionarbona <patricioinarbona@gmail.com>
2026-02-25 21:06:24 +0100

    feat: add getEjercicio

--- src/routes/ejercicios.routes.ts
+++ src/routes/ejercicios.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/authJwt";
-import { addEjercicio } from "../controllers/ejercicios.controller";
+import { addEjercicio, getEjercicio } from "../controllers/ejercicios.controller";
 
 const router = Router();
 
 router.post("/", verifyToken, addEjercicio); //TODO: add role
+router.get("/", getEjercicio);
 
 export default router;
