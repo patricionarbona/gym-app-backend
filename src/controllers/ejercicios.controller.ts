7d0fae1dac7abce786ee6a120c8f0d5dfb69113f
patricionarbona <patricioinarbona@gmail.com>
2026-02-25 21:49:15 +0100

    feat: controller to get ejercicios

--- src/controllers/ejercicios.controller.ts
+++ src/controllers/ejercicios.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { CreateEjercicioParams } from "../interfaces";
-import { createEjercicio } from "../services/database.service";
+import {
+  createEjercicio,
+  getEjercicios as getEjerciciosDB,
+} from "../services/database.service";
 
 export const addEjercicio = async (req: Request, res: Response) => {
   const newEjercicio: CreateEjercicioParams = req.body;
@@ -9,4 +12,13 @@ export const addEjercicio = async (req: Request, res: Response) => {
     res.status(400).json({ message: result.message });
   }
   return res.status(201).json({ message: result.message });
+};
+
+export const getEjercicios = async (req: Request, res: Response) => {
+  const result = await getEjerciciosDB();
+  if (!result.ok) {
+    return res.status(400).json({ message: result.message });
+  } else {
+    return res.status(200).json({ result: result.result });
+  }
 };
