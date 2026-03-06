85300c740a58fc6f8c943e18f297578ad35f5b65
patricionarbona <patricioinarbona@gmail.com>
2026-03-06 19:14:25 +0100

    chore: delete log

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -22,7 +22,6 @@ export const foundUserById = async (id: number): Promise<UserDB | null> => {
   try {
     const query = "SELECT * FROM usuarios WHERE id = ?";
     const [rows] = await pool.execute<UserDB[]>(query, [id]);
-    console.log(rows);
     return rows.length ? rows[0] : null;
   } catch (err: any) {
     console.log("DB Error:", err.message);
